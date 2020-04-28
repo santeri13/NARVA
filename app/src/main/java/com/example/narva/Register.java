9e9f90ed5acc1a03d994b099eb46c7ef90ce8c53
santeri13 <Samulipoh>
2020-04-28 15:15:02 +0300

    Project alpha version

---
+++ app/src/main/java/com/example/narva/Register.java
@@ -0,0 +1,85 @@
+package com.example.narva;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Register extends AppCompatActivity {
+
+    Button button;
+    private EditText email,password,nickname;
+    private FirebaseAuth firebaseAuth;
+    DatabaseReference databsereference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Window g = getWindow();
+        g.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.TYPE_STATUS_BAR);
+        button = (Button) findViewById(R.id.Enter);
+        firebaseAuth = FirebaseAuth.getInstance();
+        databsereference = FirebaseDatabase.getInstance().getReference("user");
+        email = findViewById(R.id.e_mail);
+        password = findViewById(R.id.password);
+        nickname = findViewById(R.id.name_enter);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Email = email.getText().toString();
+                String Password = password.getText().toString();
+                String Name = nickname.getText().toString();
+                if(Email.isEmpty()){
+                    email.setError("Please enter email id");
+                    email.requestFocus();
+                }
+                else  if(Password.isEmpty()){
+                    password.setError("Please enter your password");
+                    password.requestFocus();
+                }
+                else  if(Email.isEmpty() && Password.isEmpty()){
+                    Toast.makeText(Register.this,"Fields Are Empty!",Toast.LENGTH_SHORT).show();
+                }
+                else  if(!(Email.isEmpty() && Password.isEmpty())){
+                    firebaseAuth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(Register.this,"SignUp Unsuccessful, Please Try Again",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(Register.this,Slider.class));
+                            }
+                        }
+
+                    });
+                    addUser();
+                }
+                else{
+                    Toast.makeText(Register.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+    private void addUser(){
+        String Email = email.getText().toString();
+        String Name = nickname.getText().toString();
+        String Password = password.getText().toString();
+        String id = databsereference.push().getKey();
+        RegisterDatabase registerDatabase = new RegisterDatabase(id,Name,Email,Password);
+        databsereference.child(Name).setValue(registerDatabase);
+    }
+}
---
+++ app/src/main/java/com/example/narva/RegisterDatabase.java
@@ -0,0 +1,35 @@
+package com.example.narva;
+
+public class RegisterDatabase {
+
+    String id;
+    String username;
+    String email;
+    String password;
+
+    public RegisterDatabase(){
+
+    }
+
+    public RegisterDatabase(String id, String username, String email,String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
+}
